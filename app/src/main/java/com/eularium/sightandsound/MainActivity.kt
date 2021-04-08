28df9bef2faaa72922845c8ebf4995f0fa444590
Kieran Jordine <kieranjordine@gmail.com>
2021-04-07 21:52:42 -0500

    added dependencies

--- app/src/main/java/com/eularium/sightandsound/MainActivity.kt
+++ app/src/main/java/com/eularium/sightandsound/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.eularium.sightandsound
 
+import android.Manifest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.eularium.sightandsound.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private const val PERMISSIONS_REQUEST_CODE = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        startActivity(Intent(this, FullscreenActivity::class.java))
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
